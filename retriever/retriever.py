0bb0b7fb6848b37c64fd651ad1bfcb807d5794af
Folkert van Heusden <folkert@vanheusden.com>
2026-01-04 20:47:41 +0100

    retriever: recover from timeouts

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -18,22 +18,33 @@ def setup_db(db_file):
     con.close()
 
 def worker(address, db_file):
-    con = sqlite3.connect(db_file)
+    while True:
+        try:
+            con = sqlite3.connect(db_file)
 
-    uri = f'ws://{address}/ws'
-    with connect(uri) as websocket:
-        while True:
-            packet = websocket.recv()
-            if packet == None:
-                break
+            uri = f'ws://{address}/ws'
+            with connect(uri) as websocket:
+                while True:
+                    packet = websocket.recv()
+                    if packet == None:
+                        break
 
-            cur = con.cursor()
-            try:
-                cur.execute('INSERT INTO packets(data) VALUES(?)', (packet,))
-                con.commit()
-            except Exception as e:
-                print(f'Failed inserting packet: {e}')
-            cur.close()
+                    cur = con.cursor()
+                    try:
+                        cur.execute('INSERT INTO packets(data) VALUES(?)', (packet,))
+                        con.commit()
+                    except Exception as e:
+                        print(f'Failed inserting packet: {e}')
+                    cur.close()
+
+        except TimeoutError:
+            pass
+
+        except Exception as e:
+            print(f'worker: {e}')
+            raise e
+
+    con.close()
 
 if __name__ == "__main__":
     setup_db(config.db_file)
