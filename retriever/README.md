6a7991e89ba0e619693cadd0e784f718a4c219b7
Folkert van Heusden <folkert@vanheusden.com>
2026-01-04 18:29:45 +0100

    1st commit of retriever

---
+++ retriever/README.md
@@ -0,0 +1,2 @@
+* install dependencies from requirements.txt in a venv
+* run retriever.py
---
+++ retriever/retriever.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+from websockets.sync.client import connect
+import config
+import sqlite3
+
+
+def setup_db(db_file):
+    con = sqlite3.connect(db_file)
+    cur = con.cursor()
+    try:
+        cur.execute('PRAGMA journal_mode=wal')
+        cur.execute('CREATE TABLE packets(id INTEGER PRIMARY KEY AUTOINCREMENT, ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, data BLOB NOT NULL, channel TEXT)')
+        con.commit()
+    except:
+        pass
+    cur.close()
+    con.close()
+
+def worker(address, db_file):
+    con = sqlite3.connect(db_file)
+
+    uri = f'ws://{address}/ws'
+    with connect(uri) as websocket:
+        while True:
+            packet = websocket.recv()
+            if packet == None:
+                break
+
+            cur = con.cursor()
+            try:
+                cur.execute('INSERT INTO packets(data) VALUES(?)', (packet,))
+                con.commit()
+            except Exception as e:
+                print(f'Failed inserting packet: {e}')
+            cur.close()
+
+if __name__ == "__main__":
+    setup_db(config.db_file)
+    worker(config.radio_hostname, config.db_file)
